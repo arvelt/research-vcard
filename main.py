ec12e0ac342808376c9b5dc5e10c96b61560908a
Keisuke Ota <arvelt.s@gmail.com>
2017-08-17 21:01:50 +0900

    tweak

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ SAMPLE_VCF = u"""
 BEGIN:VCARD
 VERSION:3.0
 PRODID:-//Apple Inc.//iPhone OS 10.2//EN
-N:管理;太郎;;;
-FN: 太郎  管理
+N:管理;太郎５;;;
+FN: 太郎５  管理
 X-PHONETIC-FIRST-NAME:たろう
 X-PHONETIC-LAST-NAME:かんり
 ORG:会社名;
@@ -17,6 +17,7 @@ X-PHONETIC-ORG:かいしゃめい
 EMAIL;type=INTERNET;type=WORK;type=pref:admin@example.com
 item1.URL;type=pref:https://example.com
 item1.X-ABLabel:_$!<HomePage>!$_
+UID:5743e92e-8a6a-4d6d-a095-6dfdd51543d4
 END:VCARD
 """.strip()
 
